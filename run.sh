7d334819dc9610df41fec5daf8de397601551950
Rob Sletten <rsletten@gmail.com>
2019-03-09 12:08:36 -0500

    fixed networking but still have SSL error

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
-docker run -it -p 8000:80 -v  /home/rsletten/pupperware/volumes/puppetdb/ssl:/etc/puppetlabs/puppet/ssl \
-  -e PUPPETDB_HOST=localhost \
+docker run -it -p 8000:80 --network='pupperware_default' -v /home/rsletten/pupperware/volumes/puppetdb/ssl:/etc/puppetlabs/puppetdb/ssl \
+  -e PUPPETDB_HOST=puppetdb \
   -e PUPPETDB_PORT=8081 \
-  -e PUPPETDB_SSL_VERIFY=/home/rsletten/pupperware/volumes/puppetdb/ssl/certs/ca.pem \
-  -e PUPPETDB_KEY=/home/rsletten/pupperware/volumes/puppetdb/ssl/private_keys/puppetdb.pem \
-  -e PUPPETDB_CERT=/home/rsletten/pupperware/volumes/puppetdb/ssl/public_keys/puppetdb.pem \
+  -e PUPPETDB_SSL_VERIFY=/etc/puppetlabs/puppetdb/ssl/certs/ca.pem \
+  -e PUPPETDB_KEY=/etc/puppetlabs/puppetdb/ssl/private_keys/puppetdb.pem \
+  -e PUPPETDB_CERT=/etc/puppetlabs/puppetdb/ssl/public_keys/puppetdb.pem \
   -e INVENTORY_FACTS='Hostname,fqdn, IP Address,ipaddress' \
   -e ENABLE_CATALOG=True \
   -e GRAPH_FACTS='architecture,puppetversion,osfamily' \
